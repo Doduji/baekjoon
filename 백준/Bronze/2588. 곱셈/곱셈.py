26e337d6cbfae7061455c7dd1bc37786787b2b55
Doduji <52474696+Doduji@users.noreply.github.com>
2023-03-22 22:56:43 +0900

    [Bronze III] Title: 곱셈, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2588. 곱셈/곱셈.py
@@ -0,0 +1,11 @@
+fir = int(input())
+sec = int(input())
+
+sec_list =[]
+for i in str(sec) :
+    sec_list.append(int(i))
+
+print(sec_list[2]*fir)
+print(sec_list[1]*fir)
+print(sec_list[0]*fir)
+print(fir*sec)
