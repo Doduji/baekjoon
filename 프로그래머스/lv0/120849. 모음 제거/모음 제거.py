41f63bda46fbe2d66544ad81575494c62c4daa65
Doduji <52474696+Doduji@users.noreply.github.com>
2023-07-20 16:48:53 +0900

    [level 0] Title: 모음 제거, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.py
@@ -0,0 +1,11 @@
+def solution(my_string):
+    answer = ''
+    
+    my_string = my_string.replace('a', '')
+    my_string = my_string.replace('i', '')
+    my_string = my_string.replace('u', '')
+    my_string = my_string.replace('e', '')
+    my_string = my_string.replace('o', '')
+    answer= my_string
+    
+    return answer
