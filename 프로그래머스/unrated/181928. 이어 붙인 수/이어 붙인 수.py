fb012bede9dc64a62338035cddee8033eb433bcf
Doduji <52474696+Doduji@users.noreply.github.com>
2023-07-28 20:19:43 +0900

    [unrated] Title: 이어 붙인 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181928. 이어 붙인 수/이어 붙인 수.py
@@ -0,0 +1,13 @@
+def solution(num_list):
+    answer = 0
+    hol=""
+    jak=""
+    for i in range(0, len(num_list)) :
+        if num_list[i]%2==0 :
+            jak=jak+ str(num_list[i])
+        else :
+            hol=hol+str(num_list[i])
+    
+    answer=int(hol)+int(jak)
+    
+    return answer
